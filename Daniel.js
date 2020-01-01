c05fa74dbb9b5d36eeacfd364013e58f3ce74092
Daniel <daniel22bar@gmail.com>
2020-01-01 13:43:19 +0200

    new commit

--- Daniel.js
+++ Daniel.js
@@ -1,11 +1,9 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-
 function GET(url) {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, false);
     xhttp.send();
     return JSON.parse(xhttp.responseText);
 }
-
+//sdfgsdfgsdf
 console.log(GET('https://jsonplaceholder.typicode.com/todos/1'));
